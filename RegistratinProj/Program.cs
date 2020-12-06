9b82d988d27f7331762a6d14b7a9ee2dc9c23a74
Taria <63594053+Taria@users.noreply.github.com>
2020-12-06 15:10:04 +0200

    Add files via upload

---
+++ RegistratinProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegistratinML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RegistratinProj/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistratinML
+{
+    public class Registration
+    {
+        public int Login(string username, string password)
+        {
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password) || username.Length < 8 || username.Length > 30
+                || password.Length < 8 || password.Length > 30)
+                throw new ArgumentOutOfRangeException("Incorect input field");
+
+            return 0;
+        }
+    }
+}
